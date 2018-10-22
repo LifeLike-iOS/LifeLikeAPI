30802c456b079f2b4c2735772dcf522ba9c8b99f
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro-2.local>
2018-10-22 11:07:30 -0400

    mongo connection setup

--- lifelike.py
+++ lifelike.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
-import connexion
+# import connexion
+from flask import Flask
+import pymongo
+import urllib 
+app = Flask(__name__)
 
+username = urllib.parse.quote_plus('tiffanychen@cmu.edu')
+password = urllib.parse.quote_plus('benjin!56fan')
+client = pymongo.MongoClient("mongodb+srv://%s:%s@lifelike-cluster-rialo.mongodb.net/test?retryWrites=true"  % (username, password))
+db = client.test
 
-def post_greeting(name: str) -> str:
-    return 'Hello {name}'.format(name=name)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, specification_dir='./')
-    # app = Flask(__name__)
-    app.add_api('swagger.yaml', arguments={'title': 'Hello World Example'})
-    app.run()
+# if __name__ == '__main__':
+#     app = connexion.FlaskApp(__name__, specification_dir='./')
+#     # app = Flask(__name__)
+#     app.add_api('swagger.yaml', arguments={'title': 'Hello World Example'})
+#     app.run()
