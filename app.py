f7551e00452b2c49bfb1cdfd5adc621bb5c55bbe
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro-2.local>
2018-10-23 17:43:37 -0400

    Swagger docs details added

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ from flasgger import Swagger
 # APP SET UP
 app = Flask(__name__)
 app.config['SWAGGER'] = {
-    'title': 'Lifelike Books API'
+    'title': 'Lifelike Books API',
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "model_filter": lambda tag: True,  # all in
+        }
+    ]
 }
 swagger = Swagger(app)
 # app = connexion.FlaskApp(__name__, specification_dir='./')
@@ -28,6 +35,9 @@ def health_check():
 # List all books
 @app.route('/books', methods=['GET'])
 def get_all_books():
+    """
+    file: ./docs/get_all_books.yml
+    """
     try:
         books = collection.find()
         return json_util.dumps(books), 200
@@ -37,6 +47,9 @@ def get_all_books():
 # Text search for a book
 @app.route('/books/search/<string:search_param>', methods=["GET"])
 def search_for_book(search_param):
+    """
+    file: ./docs/search_for_book.yml
+    """
     try: 
         results = search_by_keys(search_param)
         return json_util.dumps(results), 200
@@ -46,6 +59,9 @@ def search_for_book(search_param):
 # Retrieve a book
 @app.route('/books/<string:id>', methods=['GET'])
 def get_book(id):
+    """
+    file: ./docs/get_book.yml
+    """
     try:
         book = collection.find_one({"_id": ObjectId(id)})
         return json_util.dumps(book), 200
@@ -55,6 +71,9 @@ def get_book(id):
 # Add book
 @app.route('/books/add', methods=['POST'])
 def add_book():
+    """
+    file: ./docs/add_book.yml
+    """
     try:
         book_data = request.json
         collection.insert_one(book_data)
@@ -65,6 +84,9 @@ def add_book():
 # Edit book 
 @app.route('/books/<string:id>/edit', methods=['PUT'])
 def edit_book(id):
+    """
+    file: ./docs/edit_book.yml
+    """
     try:
         updated_content = request.json
         book = collection.update({"_id": ObjectId(id)},{"$set": updated_content})
@@ -76,6 +98,9 @@ def edit_book(id):
 # Delete book
 @app.route('/books/<string:id>/delete', methods=['DELETE'])
 def delete_book(id):
+    """
+    file: ./docs/delete_book.yml
+    """
     try:
         deleted_book = collection.find_one({"_id": ObjectId(id)})
         collection.delete_one({"_id": ObjectId(id)})
@@ -90,7 +115,8 @@ def search_by_keys(search_param):
     books = collection.find()
     results = []
     for book in books:
-        print(book)
+        print(book['title'])
+        print(search_param in book['title'])
         if search_param in book['title'] or search_param in book['authors']:
             results.append(book)
         elif 'ISBN_13' in book and search_param == book['ISBN_13']:
@@ -101,8 +127,6 @@ def error_message(message):
     return json.dumps({'success': False, 'message': message}), 400
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     # app.run()
-    # app.add_api('swagger.yaml', arguments={'title': 'Lifelike Book API'})
-    app.run()
