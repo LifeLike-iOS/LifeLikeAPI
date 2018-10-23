f8e7c932299f09ec5a389b48a9b4c1942e4b7f29
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro-2.local>
2018-10-23 00:05:03 -0400

    baseline api crud interactions done

--- lifelike.py
+++ lifelike.py
@@ -1,15 +1,53 @@
-#!/usr/bin/env python3
-
-# import connexion
-from flask import Flask
-app = Flask(__name__)
-
+# LIBRARIES
+from flask import Flask, request
+from bson import json_util, ObjectId
 import pymongo
+import json
+# import connexion
+
+# APP SET UP
+app = Flask(__name__)
 url = "mongodb://_:%s@stitch.mongodb.com:27020/?authMechanism=PLAIN&authSource=%sexternal&ssl=true&appName=lifelike-biukf:mongodb-atlas:api-key" % ('pE1r3IXYjPGmySQ3fSOT8Fn9ishrFrj6nREzPnFXyy2pEKQvH3DYX5D3jTbgcOzL', '%24')
 client = pymongo.MongoClient(url)
 db = client.lifelike
 collection = db.books
 
+
+# ROUTES
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def health_check():
+    return 'Heathly!', 200
+
+# List all books
+@app.route('/books', methods=['GET'])
+def get_all_books():
+    books = collection.find()
+    return json_util.dumps(books), 200
+
+# Retrieve a book
+@app.route('/books/<string:id>', methods=['GET'])
+def get_book(id):
+    book = collection.find_one({"_id": ObjectId(id)})
+    return json_util.dumps(book), 200
+
+# Add book
+@app.route('/books/add', methods=['POST'])
+def add_book():
+    book_data = request.json
+    collection.insert_one(book_data)
+    return json.dumps({'success': True, 'message': book_data['title'] + ' added!'}), 200
+
+# Edit book 
+@app.route('/books/<string:id>/edit', methods=['PUT'])
+def edit_book(id):
+    updated_content = request.json
+    book = collection.update({"_id": ObjectId(id)},{"$set": updated_content})
+    edited_book = collection.find_one({"_id": ObjectId(id)})
+    return json.dumps({'success': True, 'message': edited_book['title'] + ' updated!'}), 200
+
+# Delete book
+@app.route('/books/<string:id>/delete', methods=['DELETE'])
+def delete_book(id):
+    deleted_book = collection.find_one({"_id": ObjectId(id)})
+    collection.delete_one({"_id": ObjectId(id)})
+    return json.dumps({'success': True, 'message': deleted_book['title'] + ' deleted!'}), 200
--- mongo_test.py
+++ mongo_test.py
@@ -12,38 +12,41 @@ fs = gridfs.GridFS(db)
 
 collection = db.books
 #collection.delete_one({"_id": ObjectId('5bce3bf3a9a32a4785dfff2c')})
-imageFile = open('assets/coffeepot.jpg', 'r')
-image = fs.put(imageFile)
 # test_book = {
-#                 "title": "The Design of Everyday Things",
-#                 "authors": ["Donald A. Norman"],
-#                 "ISBN_13": "9780465050659",
-#                 "publisher": "Basic Books",
-#                 "publication-date": datetime.datetime(2013, 11, 5),
-#                 "page_count": 368,
-#                 "chapter_count": 7,
-#                 "chapters": [{
-#                         "chapter_num": 1,
-#                         "chapter_title": "The Psychopathology of Everyday Things",
-#                         "page_count": 33,
-#                         "pages": [
-#                             {
-#                                 "page_number": 2,
-#                                 "image_count": 1,
-#                                 "images": [
-#                                     {
-#                                         "image_title": "1.1 Carelman's Coffeepot for Masochists",
-#                                         "image_file": image
-#                                     }
-#                                 ]
-#                             }
-#                         ]
-#                     },
-#                     {
-#                         "chapter_num": 2,
-#                         "chapter_title": "The Psychology of Everyday Actions",
-#                         "page_count": 20
-#                     }
-#                 ]
+#             "title": "Test Book"
 #             }
 # collection.insert_one(test_book)
+# imageFile = open('assets/coffeepot.jpg', 'r')
+# image = fs.put(imageFile)
+test_book = {
+                "title": "The Design of Everyday Things",
+                "authors": ["Donald A. Norman"],
+                "ISBN_13": "9780465050659",
+                "publisher": "Basic Books",
+                "publication-date": datetime.datetime(2013, 11, 5),
+                "page_count": 368,
+                "chapter_count": 7,
+                "chapters": [{
+                        "chapter_num": 1,
+                        "chapter_title": "The Psychopathology of Everyday Things",
+                        "page_count": 33,
+                        "pages": [
+                            {
+                                "page_number": 2,
+                                "image_count": 1,
+                                "images": [
+                                    {
+                                        "image_title": "1.1 Carelman's Coffeepot for Masochists"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "chapter_num": 2,
+                        "chapter_title": "The Psychology of Everyday Actions",
+                        "page_count": 20
+                    }
+                ]
+            }
+collection.insert_one(test_book)
