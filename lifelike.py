94703f73e6802d83b945dc384b8c8eef5964bb72
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro-2.local>
2018-10-23 12:13:12 -0400

    added basic error catching

--- lifelike.py
+++ lifelike.py
@@ -21,45 +21,64 @@ def health_check():
 # List all books
 @app.route('/books', methods=['GET'])
 def get_all_books():
-    books = collection.find()
-    return json_util.dumps(books), 200
+    try:
+        books = collection.find()
+        return json_util.dumps(books), 200
+    except Exception as e:
+        return error_message("Failed to retreieve data!")
 
 # Text search for a book
 @app.route('/books/search/<string:search_param>', methods=["GET"])
 def search_for_book(search_param):
-    results = search_by_keys(search_param)
-    return json_util.dumps(results), 200
+    try: 
+        results = search_by_keys(search_param)
+        return json_util.dumps(results), 200
+    except Exception as e:
+        return error_message("Search failed!")
 
 # Retrieve a book
 @app.route('/books/<string:id>', methods=['GET'])
 def get_book(id):
-    book = collection.find_one({"_id": ObjectId(id)})
-    return json_util.dumps(book), 200
+    try:
+        book = collection.find_one({"_id": ObjectId(id)})
+        return json_util.dumps(book), 200
+    except Exception as e:
+        return error_message("Failed to retrieve book with id of " + id)
 
 # Add book
 @app.route('/books/add', methods=['POST'])
 def add_book():
-    book_data = request.json
-    collection.insert_one(book_data)
-    return json.dumps({'success': True, 'message': book_data['title'] + ' added!'}), 200
+    try:
+        book_data = request.json
+        collection.insert_one(book_data)
+        return json.dumps({'success': True, 'message': book_data['title'] + ' added!'}), 200
+    except Exception as e:
+        return error_message("Failed to add book!")
 
 # Edit book 
 @app.route('/books/<string:id>/edit', methods=['PUT'])
 def edit_book(id):
-    updated_content = request.json
-    book = collection.update({"_id": ObjectId(id)},{"$set": updated_content})
-    edited_book = collection.find_one({"_id": ObjectId(id)})
-    return json.dumps({'success': True, 'message': edited_book['title'] + ' updated!'}), 200
+    try:
+        updated_content = request.json
+        book = collection.update({"_id": ObjectId(id)},{"$set": updated_content})
+        edited_book = collection.find_one({"_id": ObjectId(id)})
+        return json.dumps({'success': True, 'message': edited_book['title'] + ' updated!'}), 200
+    except Exception as e:
+        return error_message("Failed to update book with id of " + id)
 
 # Delete book
 @app.route('/books/<string:id>/delete', methods=['DELETE'])
 def delete_book(id):
-    deleted_book = collection.find_one({"_id": ObjectId(id)})
-    collection.delete_one({"_id": ObjectId(id)})
-    return json.dumps({'success': True, 'message': deleted_book['title'] + ' deleted!'}), 200
+    try:
+        deleted_book = collection.find_one({"_id": ObjectId(id)})
+        collection.delete_one({"_id": ObjectId(id)})
+        return json.dumps({'success': True, 'message': deleted_book['title'] + ' deleted!'}), 200
+    except Exception as e:
+        return error_message("Failed to delete book with id of " + id)
 
 
 # ADDITIONAL METHODS
+
 # Search function because pymongo indexing is failing
 def search_by_keys(search_param):
     books = collection.find()
@@ -71,3 +90,6 @@ def search_by_keys(search_param):
         elif 'ISBN_13' in book and search_param == book['ISBN_13']:
             results.append(book)
     return results
+
+def error_message(message):
+    return json.dumps({'success': False, 'message': message}), 400
