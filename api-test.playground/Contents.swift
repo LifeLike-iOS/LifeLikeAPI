9c8af97f1e617b9d9d3485719b85aef01933a116
Devin Fan <dbfan@andrew.cmu.edu>
2018-10-24 10:59:36 -0400

    Updated swift playground to hit all api endpoints

--- api-test.playground/Contents.swift
+++ api-test.playground/Contents.swift
@@ -1,10 +1,54 @@
 import UIKit
 import Foundation
 
-let urlString = "https://lifelike-api.herokuapp.com/books/5bcf685ba9a32a8279da36b1"
- let defaultSession = URLSession(configuration: .default)
+let defaultSession = URLSession(configuration: .default)
 
-if let url = URL(string: urlString) {
+// Gets one book with endpoint /books/[:id]
+if let url = URL(string: "https://lifelike-api.herokuapp.com/books/5bcf685ba9a32a8279da36b1") {
+    let dataTask = defaultSession.dataTask(with: url) { (data, response, error) in
+        if let error = error {
+            print(error)
+        }
+        else if let data = data,
+            let response = response as? HTTPURLResponse,
+            response.statusCode == 200 {
+            do {
+                let dict = try JSONSerialization.jsonObject(with: data, options: [])
+                print(dict)
+            } catch {
+                print(error)
+            }
+            
+        }
+    }
+    
+    dataTask.resume()
+}
+
+// Gets books with endpoint /books/search/[:search_param]
+if let url = URL(string: "https://lifelike-api.herokuapp.com/books/search/Arduino%20Projects%20Book") {
+    let dataTask = defaultSession.dataTask(with: url) { (data, response, error) in
+        if let error = error {
+            print(error)
+        }
+        else if let data = data,
+            let response = response as? HTTPURLResponse,
+            response.statusCode == 200 {
+            do {
+                let dict = try JSONSerialization.jsonObject(with: data, options: [])
+                print(dict)
+            } catch {
+                print(error)
+            }
+            
+        }
+    }
+    
+    dataTask.resume()
+}
+
+// Gets all books with endpoint /books
+if let url = URL(string: "https://lifelike-api.herokuapp.com/books") {
     let dataTask = defaultSession.dataTask(with: url) { (data, response, error) in
         if let error = error {
             print(error)
